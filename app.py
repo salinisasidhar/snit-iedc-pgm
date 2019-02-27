20f3ad65004123edbd30662e68f017fb59172639
salinisasidhar@gmail.com <salinisasidhar@gmail.com>
2019-02-27 12:46:59 +0530

    web  program1"

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
 app=Flask(__name__)
+@app.route("/")
+def index():
+    return "<h1>hello...</h1>"
+@app.route("/home")
+def home():
+    return "Welcome to my home page"
+@app.route("/about")
+def about():
+    return "About as page"
+@app.route("/contact")
+def contact():
+    return "Contact Us page"
 if(__name__=="__main__"):
-    app.run()
-    
+    app.run(debug=True)
