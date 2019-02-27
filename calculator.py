b11a5168517c67dfa38af3f0819d0b9845e625aa
salinisasidhar@gmail.com <salinisasidhar@gmail.com>
2019-02-27 11:44:30 +0530

    module3 Pgm

--- calculator.py
+++ calculator.py
@@ -4,3 +4,5 @@ def add(x,y):
 def sub(x,y):
     z=x-y
     print(z)
+def testing():
+    print(__name__)
--- module1.py
+++ module1.py
@@ -3,4 +3,5 @@ a=123
 b=78
 calculator.add(a,b)
 calculator.sub(a,b)
+calculator.testing()
 print(__name__)
